d08376e3a7023ed949f0e17b1db7d9b2e355ea89
OzanUlus <ilkerozanulus@gmail.com>
2025-07-09 17:36:07 +0300

    configuration

--- OU.Microservice.Order.Domain/Entities/Order.cs
+++ OU.Microservice.Order.Domain/Entities/Order.cs
@@ -12,7 +12,7 @@ namespace OU.Microservice.Order.Domain.Entities
         public int AddressId { get; set; }
         public decimal TotalPrice { get; set; }
         public float? DiscountRate { get; set; }
-        public Guid PaymentId { get; set; }
+        public Guid? PaymentId { get; set; }
 
         public List<OrderItem> OrderItems { get; set; } = new();
         public Address Address { get; set; } = null!;
