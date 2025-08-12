1eb34d2ec9b8c5d30c1b9ac88ef0140d3bfed37b
OzanUlus <ilkerozanulus@gmail.com>
2025-08-12 16:39:44 +0300

    gateway(yarp)

--- OU.MicroService.Gateway/Program.cs
+++ OU.MicroService.Gateway/Program.cs
@@ -1,8 +1,15 @@
+using OU.Microservice.Shared.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthenticationAndAuthorizationExt(builder.Configuration);
 var app = builder.Build();
 app.MapReverseProxy();
 
 app.MapGet("/", () => "YARP (Gateway)");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
